c56f4fcdbbdf45a54f84d7fb24e98742a2fbfa7b
Nick Zart <nzart@g.clemson.edu>
2020-09-07 12:34:28 -0400

    HeroCard add Media query

--- src/components/Herocard.js
+++ src/components/Herocard.js
@@ -11,7 +11,8 @@ const Herocard = (props) => {
             <img src={props.imgSrc} alt={props.altTag} />
 
             <div className='card_desc'>
-                <h1>{props.title}</h1>
+                <h2>{props.title}</h2>
+                {/* <h1>{props.title}</h1> */}
                 {/* <h2>Hello</h2> */}
                 <p>{props.desc}</p>
             </div>
