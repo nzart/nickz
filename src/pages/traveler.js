f7d2a49586cb5d68b4e5714f3066d98de85e612b
Nick Zart <nzart@g.clemson.edu>
2020-09-08 16:08:51 -0400

    Traveler styling and content

--- src/pages/traveler.js
+++ src/pages/traveler.js
@@ -5,6 +5,8 @@ import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Textandhead from '../components/Textandhead';
 
+import hero from 'assets/images/traveler/travelhero.png';
+
 const TravelerPage = () => {
   return (
     <Layout pageName="traveler">
@@ -13,10 +15,30 @@ const TravelerPage = () => {
       </Helmet>
       <Container>
 
-      <h1>Traveler</h1>
-      <h3>App Design</h3>
+        <div className="wrapper">
 
-      <Textandhead head="Initial Wireframe" para="The first idea was to have users choose a city, select a few interests and have the app create a custom itinerary. While designing the wireframes the app seemed to lack any substance. While it would be a custom itinerary, there was not much room for the users to grow. They would be relying on an app too much and wouldn't have an opportunity to change how they travel. Like travel, technology should help us grow." />
+          <div className="heroText">
+            <h1>Traveler</h1>
+            <h2>The Itinerary App</h2>
+          </div>
+
+        </div>
+
+        
+
+          <img src={hero} alt="Travler hero" className="heroImg" />
+          {/* <img src={hero} alt="Travler hero" className="heroImg" /> */}
+        <div className="wrapper">  
+
+          <Textandhead head="The Problem" para="Traveling can provide unique world views and is an opportunity for people to experience a new city, region, state, country, or continent. People can get caught up with planning and end up frustrated trying to create a plan." />
+
+          <Textandhead head="Initial Wireframe" para="The first idea was to have users choose a city, select a few interests and have the app create a custom itinerary. While designing the wireframes the app lacked any substance. While it would be a custom itinerary, there was not much room for the users to grow. They would be relying on an app too much and wouldn't have an opportunity to change how they travel. Like travel, technology should help us grow." />
+
+          <Textandhead head="Final Wireframe" para="The final wireframes had users choose a city and then they could either browse itineraries or activities. Users could also create their own itinerary." />
+
+          <Textandhead head="HiFi" para="The final designs used a lot of pictures for a reason. By using as many pictures as possible, users would see what cities have to offer and would be inspired to travel." />
+
+        </div>
 
       </Container>
     </Layout>
