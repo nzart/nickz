38a97c5c774104d39f7fa236afff927c173d0b6e
Nick Zart <nzart@g.clemson.edu>
2020-09-10 11:26:33 -0400

    Added more cards and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,12 +26,13 @@ const IndexPage = () => {
 
             <div className="Herogallery">
               <Herocard page="/traveler" imgSrc={nyc} altTag="New Yawk" title="Traveler" desc="App Design" />
+              <Herocard page="/" imgSrc={mount} altTag="Mountain" title="Temple" desc="Branding" />
               <Herocard page="/" imgSrc={mount} altTag="Mountain" title="Crown Of Red" desc="Branding" />
+              <Herocard page="/" imgSrc={mount} altTag="Mountain" title="Fitness" desc="App Design" />
+              <Herocard page="/" imgSrc={mount} altTag="Mountain" title="About" desc="My Life" />
+              <Herocard page="/" imgSrc={mount} altTag="Mountain" title="Contact" desc="Slide in the DMs" />
             </div>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet non curabitur gravida arcu ac. Et leo duis ut diam quam nulla porttitor massa id. Mi eget mauris pharetra et. Consectetur purus ut faucibus pulvinar. Ut diam quam nulla porttitor massa id neque aliquam. Facilisi cras fermentum odio eu. Libero id faucibus nisl tincidunt eget nullam. Ut aliquam purus sit amet luctus. Gravida dictum fusce ut placerat orci nulla pellentesque. Laoreet id donec ultrices tincidunt arcu non sodales. Pretium viverra suspendisse potenti nullam. Mattis aliquam faucibus purus in massa tempor nec. Ultrices sagittis orci a scelerisque. Tristique nulla aliquet enim tortor at auctor. Lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis. Amet est placerat in egestas erat imperdiet.
-            </p>
 
         </div>
       </Container>  
